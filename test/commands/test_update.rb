2549ffba91ce09038d79c9a1e2ff24aaa0fa164b
Matt Pruitt <guitsaru@gmail.com>
2009-06-23 21:37:25 -0500

    Fixed some tests.
    [#2 tagged:'branch:2-create-command-classes']

--- test/commands/test_update.rb
+++ test/commands/test_update.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class TestBranchUpdate < Test::Unit::TestCase
   should "Update the ticket" do
-    assert_equal("git commit -a -m 'Message.\n\n[#1 tagged:\"branch:1-test-ticket\"]'",
+    assert_equal("git commit -a -m \"Message.\n\n[#1 tagged:'branch:1-test-ticket']\"",
       Command::Update.command_string('1-test-ticket', 1, ['Message.']))
   end
   
   should "Update the ticket with extra" do
-    assert_equal("git commit -a -m 'Message.\n\n[#1 tagged:\"branch:1-test-ticket\" milestone:'next milestone']'",
+    assert_equal("git commit -a -m \"Message.\n\n[#1 tagged:'branch:1-test-ticket' milestone:'next milestone']\"",
       Command::Update.command_string('1-test-ticket', 1, ['Message.', "milestone:'next milestone'"]))
   end
   
